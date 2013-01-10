60477c4b59cbdbcb0158e4a85c1fe4eedb9bf269
Kenichi Kanai <kenichi.kanai@gmail.com>
2013-01-10 18:04:17 +0900

    rails initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FluentdTest::Application.config.secret_token = 'f04e58b4cce91698bb573ede7fd11acd21eb47aeea4ad2a961a1e971966c1495146da67adcf206af34b1d7fe33260d7ee378b4e1778020cb4acb7ab13b0d6ace'
